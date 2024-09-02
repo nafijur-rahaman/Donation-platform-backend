ab7c9de55cc850c97e5767fcc7c1f58fc41dd9ae
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-09-02 22:50:58 +0600

    change localhost to deploy link

--- users/views.py
+++ users/views.py
@@ -35,7 +35,7 @@ class UserRegistrationView(generics.CreateAPIView):
         user=serializer.save()
         token=default_token_generator.make_token(user)
         uid=urlsafe_base64_encode(force_bytes(user.pk))
-        confirm_link=f"http://127.0.0.1:8000/api/users/activate/{uid}/{token}/"
+        confirm_link=f"https://donation-platform-backend-rmqk.onrender.com/api/users/activate/{uid}/{token}/"
         email_subject="Confrim You Registration"
         email_body=render_to_string('confirm_email.html',{'confirm_link':confirm_link})
         email=EmailMultiAlternatives(email_subject,'',to=[user.email])
@@ -68,9 +68,9 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active=True 
         user.save()
-        return redirect('http://127.0.0.1:5500/login.html')
+        return redirect('https://donation-platform.netlify.app/login.html')
     else:
-        return redirect('register')
+        return redirect('https://donation-platform.netlify.app/register.html')
     
 class UserLoginView(APIView):
     
