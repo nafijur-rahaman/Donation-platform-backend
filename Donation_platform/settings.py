200eeea621fe7ae8a965cf7719a176bceda24bec
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-09-23 19:19:41 +0600

    setting change

--- Donation_platform/settings.py
+++ Donation_platform/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 AUTH_USER_MODEL = 'users.User'
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -65,6 +66,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -89,7 +92,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Donation_platform.wsgi.application'
+WSGI_APPLICATION = 'Donation_platform.wsgi.app'
 
 
 # Database
@@ -103,12 +106,24 @@ WSGI_APPLICATION = 'Donation_platform.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://donationplatform_user:zWSjqIjUn0G2Fzrl6k9JtNP45nNpHIBg@dpg-cratctdds78s73d9tmjg-a.oregon-postgres.render.com/donationplatform',
+#         conn_max_age=600
+#     )
+# }
+
+
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://donationplatform_user:zWSjqIjUn0G2Fzrl6k9JtNP45nNpHIBg@dpg-cratctdds78s73d9tmjg-a.oregon-postgres.render.com/donationplatform',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.fhdltlxcfhiakcgopxaz',
+        'PASSWORD': 'SKf6Ky7k*Brf8Mh',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543'
+    }
 }
 
 
@@ -186,6 +201,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
